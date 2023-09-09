1574ff0726a84df692f669357d3c7318eb61325e
Anahi Hull <anahi.hull@cetys.edu.mx>
2023-09-09 10:03:26 -0700

    mapa en gps

--- src/views/GpsView.js
+++ src/views/GpsView.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
 
 const GpsView = () => {
   return (
-    <View>
-      <Text>GpsView View</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+          title="Mi Marcador"
+          description="Este es mi marcador"
+        />
+      </MapView>
     </View>
   );
-}
+};
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
 export default GpsView;
