a50d4a7d153d0658a3dda2cb37af01bc1d23cda1
JordiJimenezZarate <SoporteDanielZ@outlook.com>
2023-09-08 23:14:42 -0700

    Cambie el titulo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Alzheimer</Text>
+      <Text>Guten Abend, bis samstag</Text>
       <StatusBar style="auto" />
     </View>
   );
