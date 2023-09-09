5f23d74ed4fed1d652abbcc6f86ead5cfe7b9f75
atofo <atofito@hotmail.com>
2023-09-08 23:43:08 -0700

    da la orden viejo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Amo a los judios</Text>
+      <Text>Tranquilos</Text>
       <StatusBar style="auto" />
     </View>
   );
